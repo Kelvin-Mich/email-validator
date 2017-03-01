a16bb7f61c23ebbef28433e3ef7f276fef055526
mich <mich@parrot>
2017-03-01 11:57:03 -0400

    Stage 1: Create rules one and two on the email validator
    	Stage 1.1: The email string must have a single '@' character
    	Stage 1.2: The email string must have atleast one '.' character

---
+++ src/main/java/com/example/email_validator/App.java
@@ -0,0 +1,35 @@
+package com.example.email_validator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ){
+        //App jMyApp = new App();
+    	//String jMyEmail = "MYNAME@DOMAIN.COM";
+    	
+        //jMyApp.jEmailValidator(jMyEmail);
+    }
+    
+    //Email validator method
+    public void jEmailValidator(String jEmail){
+    	//Rule 1: The string has a single '@' character
+    	//Rule 2: The string has at least one '.' character
+    	// Regex string [A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}
+    	Pattern jPattern = Pattern.compile("[A-Z]+@[A-Z]+\\.[A-Z]{2,4}");
+    	
+    	//Matching string to the pattern for a valid email address
+    	Matcher jMatcher = jPattern.matcher(jEmail.toUpperCase());
+    	
+    	if(jMatcher.matches()){
+    		System.out.println("Valid Email Address...!!!");    		
+    	} else {
+    		System.out.println("Not a Valid Email");
+    	}
+    }
+}
---
+++ src/test/java/com/example/email_validator/AppTest.java
@@ -0,0 +1,60 @@
+package com.example.email_validator;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite(){
+        return new TestSuite( AppTest.class );
+    }
+    /**
+     * Rigourous Test :-)
+     */
+    
+    @org.junit.Test
+    //This test checks for a single '@' character and atleast one '.' character
+    public void testApp1()
+    {
+        App jTestApp = new App();
+        jTestApp.jEmailValidator("Your@name.ca");
+    	assertTrue( true );
+    }
+  //This test checks for a single '@' character and atleast one '.' character
+    public void testApp2()
+    {
+        App jTestApp = new App();
+        jTestApp.jEmailValidator("Yo.ur@na.me.ca");
+    	assertTrue( true );
+    }
+    //This test fails because the email does not follow the two set rules
+    public void testApp3(){
+    	App jTestApp = new App();
+    	jTestApp.jEmailValidator("Your@1name.ca");
+    	assertTrue(false);
+    }
+  //This test fails because the email does not follow the two set rules
+    public void testApp4(){
+    	App jTestApp = new App();
+    	jTestApp.jEmailValidator("Your@na@e.ca");
+    	assertTrue(false);
+    }
+}
